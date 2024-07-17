ce5b50995c2a40b3ae5622cae39f180d32435d7a
Ludovica Cocchella <84328863+LudovicaCi@users.noreply.github.com>
2024-07-17 12:42:11 +0200

    working version of GradCam

--- src/saliency_method/gradcam.py
+++ src/saliency_method/gradcam.py
@@ -1,10 +1,12 @@
+import hydra
+import os
 import torch
+from src.models.classifier import ClassifierModule
 from pytorch_grad_cam import GradCAM
 from torch import nn
-import cv2
 
 
-class gradcam_interface():
+class gradcam_interface:
     def __init__(self, model, device='cpu', **kwargs):
         self.model = model
         self.device = device
@@ -16,9 +18,17 @@ class gradcam_interface():
         if isinstance(target_layers, nn.Module):
             target_layers = [target_layers]
 
-        cam = GradCAM(self.model, target_layers)
-        saliency_maps = cam(input_image)
-        saliency_tensor = torch.from_numpy(saliency_maps)
+        # Ensure gradients are enabled and input_image requires grad
+        input_image.requires_grad = True
+
+        with torch.set_grad_enabled(True):
+            cam = GradCAM(self.model, target_layers)
+            saliency_maps = cam(input_image)
+
+        saliency_tensor = torch.from_numpy(saliency_maps).detach()
+
+        input_image.requires_grad = False
+
         return saliency_tensor
 
 
@@ -29,38 +39,57 @@ def get_layer_by_name(model, layer_name):
     raise ValueError(f"Layer '{layer_name}' not found in the model.")
 
 
-# main for testing the interface of sidu made for this project
-if __name__ == '__main__':
-    from torchvision.models import resnet34, ResNet34_Weights
+# main for testing the interface of GradCAM made for this project
+@hydra.main(config_path='../../config', config_name='config', version_base=None)
+def main(cfg):
     import matplotlib.pyplot as plt
     import torchvision.transforms as transforms
     import torchvision.datasets as datasets
     import torch.utils.data as data
 
     # Load the model and data
-    model = resnet34(weights=ResNet34_Weights.IMAGENET1K_V1)
+    model = ClassifierModule(
+        weights=cfg.model,
+        num_classes=cfg[cfg.dataset.name].n_classes,
+        finetune=cfg.train.finetune,
+        lr=cfg.train.lr,
+        max_epochs=cfg.train.max_epochs
+    )
+
+    if cfg.dataset.name != 'imagenet':
+        model_path = os.path.join(cfg.mainDir, cfg.checkpoint)
+        model.load_state_dict(torch.load(model_path, map_location=cfg.train.device)['state_dict'])
+        # model.load_state_dict(torch.load(model_path, map_location=cfg.train.device)['state_dict'])
+
     model.eval()
     transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
-    dataset = datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
-    dataloader = data.DataLoader(dataset, batch_size=1, shuffle=True)
+    dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    dataloader = data.DataLoader(dataset, batch_size=cfg.train.batch_size, shuffle=True)
 
     # Load the image
-    image, _ = next(iter(dataloader))
-    image = image.to('cpu')
+    images, _ = next(iter(dataloader))
+    images = images.to('cpu')
 
     # Initialize the Saliency method
-    target_layers_name = 'layer4.2.conv2'
+    target_layers_name = cfg.target_layers[cfg.model.split('_Weights')[0]]
 
-    method = gradcam_interface(model, device='cpu', reshape_transform=None)
-    saliency = method.generate_saliency(input_image=image, target_layer=target_layers_name)
+    method = gradcam_interface(model, device=cfg.train.device, reshape_transform=False)
+    saliency_maps = method.generate_saliency(input_image=images, target_layer=target_layers_name)
 
-    # plot the saliency map and the image
-    plt.figure(figsize=(5, 2.5))
-    plt.subplot(1, 2, 1)
-    plt.imshow(image.squeeze().permute(1, 2, 0))
-    plt.axis('off')
-    plt.subplot(1, 2, 2)
-    plt.imshow(image.squeeze().permute(1, 2, 0))
-    plt.imshow(saliency[0].squeeze().detach().numpy(), cmap='jet', alpha=0.4)
-    plt.axis('off')
-    plt.show()
+    for i in range(images.size(0)):
+        image = images[i]
+        saliency = saliency_maps[i]
+
+        plt.figure(figsize=(5, 2.5))
+        plt.subplot(1, 2, 1)
+        plt.imshow(image.squeeze().permute(1, 2, 0))
+        plt.axis('off')
+        plt.subplot(1, 2, 2)
+        plt.imshow(image.squeeze().permute(1, 2, 0))
+        plt.imshow(saliency.squeeze().detach().numpy(), cmap='jet', alpha=0.4)
+        plt.axis('off')
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
