Fix in woe probability computation, change extraction save,  implement ablation study script

extract_concept.py
@@ -1,6 +1,7 @@
 import os
 
 import cv2
+import numpy as np
 import torch
 import hydra
 import torchvision
@@ -9,7 +10,8 @@ from torchvision.transforms import ToPILImage
 
 from src.datasets.classification import load_classification_dataset
 from src.models.detector.grounded_sam2 import GroundedSam2
-from src.utils import save_mask, load_mask, retrieve_concepts, save_list, save_images_with_mask_for_all_concepts
+from src.utils import save_mask, load_mask, retrieve_concepts, save_list, save_images_with_mask_for_all_concepts, \
+    retrieve_concepts_ordered
 
 if GlobalHydra().is_initialized():
     GlobalHydra().clear()
@@ -19,6 +21,9 @@ def main(cfg):
 
     if cfg.mask.dataset:
         caption = retrieve_concepts(cfg.dataset.name)
+        print(caption)
+        list_ordered_concept = retrieve_concepts_ordered(cfg.dataset.name)
+        print(f"list ordered:{list_ordered_concept}")
         # IMAGE_PATH = "images/flower.jpg"
 
         resize = 224
@@ -34,7 +39,7 @@ def main(cfg):
         elif cfg.modelSam == 'GroundingDino':
             model = GroundedSam2(caption, 'Grounding DINO')
 
-        output_folder = os.path.join(os.path.abspath('mask_output'), cfg.dataset.name)
+        output_folder = os.path.join(os.path.abspath('mask_output'), cfg.modelSam, cfg.dataset.name)
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
 
@@ -62,8 +67,28 @@ def main(cfg):
                     os.makedirs(output_dir_tensors)
                 if not os.path.exists(output_dir_classes):
                     os.makedirs(output_dir_classes)
-                save_mask(os.path.join(output_dir_tensors, f'mask_{idx}.pth'), masks)
-                save_mask(os.path.join(output_dir_classes, f'classes_{idx}.pth'), classes) # I keep also the numpy array containing the classes
+
+                #mi carico la lista ordinata di concetti, for su questo, vedo se è presente su classes, allora lo associo, riordina
+                #ndo la maschera
+
+                masks_ordered = np.zeros(len(list_ordered_concept))
+                class_ordered = np.zeros(len(list_ordered_concept))
+
+                list_concept_ontology = model.ontology.classes()
+
+                print("lista ontology:{list_concept_ontology}")
+
+                for concept in list_ordered_concept:
+                    print(f"concept: {concept}")
+                    #make bind between ordered concept and concept of GroundedSam2, finding the class corresponding to the concept of this cycle for
+                    idx_concept = list_concept_ontology.index(concept)
+                    masks_ordered = np.append(masks_ordered, masks[idx_concept])
+                    #only for debug purpose TODO delete it
+                    class_ordered = np.append(class_ordered, classes[idx_concept])
+
+
+                save_mask(os.path.join(output_dir_tensors, f'mask_{idx}.pth'), masks_ordered)
+                #save_mask(os.path.join(output_dir_classes, f'classes_{idx}.pth'), classes) # I keep also the numpy array containing the classes
     else:
         # if cfg.mask.dataset == False, that means that we want to produce the mask for a single image
 
@@ -95,11 +120,13 @@ def main(cfg):
         boxes, masks, classes = model.mask_generation_with_all_concepts(image, resize)
 
         if len(masks) > 0:
-            output_dir = os.path.join(os.path.abspath("mask_output"), 'single_image_saliency')
+            output_dir = os.path.join(os.path.abspath("mask_output"), 'single_image')
             if not os.path.exists(output_dir):
                 os.makedirs(output_dir)
             # save_images_with_mask(boxes,masks,classes,image,model,idx) #usable in case of printing only present class mask
             save_images_with_mask_for_all_concepts(image, masks, classes, model.ontology.classes(), boxes,0, output_dir)  #to print all the class masks, even if not present
             # plot_grid_masks(image, masks, model.ontology.classes(), idx)
+
+
 if __name__ == '__main__':
     main()

src/woe.py
@@ -8,7 +8,7 @@ import pandas as pd
 import torch
 
 from src.datasets.classification import load_classification_dataset
-from src.utils import retrieve_concepts, load_list, load_mask, load_saliency_map
+from src.utils import retrieve_concepts, load_list, load_mask, load_saliency_map, retrieve_concepts_ordered
 
 
 def concept_average_saliency(mask, saliency):
@@ -27,6 +27,57 @@ def concept_average_saliency(mask, saliency):
     cas = weighted_sum / size_mask
 
     return cas
+
+def intersection_over_union(mask, saliency):
+    if mask.shape != saliency.shape:
+        print("ERROR in shape of tensors")
+
+    size_mask = torch.sum(mask)
+
+    #Avoid division by zero, if a concept is not present, return 0
+    if size_mask == 0:
+        return torch.tensor(0.0)
+
+    # prendere saliency, binarizzarla, ora sono entrambe binarie (mask e saliency) e ci faccio intersection over union
+
+    binarize_saliency = saliency > 0.5
+
+    iou = np.logical_and(binarize_saliency, mask)/ np.logical_or(binarize_saliency, mask)
+
+    return iou
+
+def concept_average_saliency_with_penalty(mask, saliency):
+    if mask.shape != saliency.shape:
+        print("ERROR in shape of tensors")
+
+    size_mask = torch.sum(mask)
+    print(size_mask)
+
+    #Avoid division by zero, if a concept is not present, return 0
+    if size_mask == 0:
+        return torch.tensor(0.0)
+
+    mask_not = np.logical_not(mask)
+
+    size_mask_not = torch.sum(mask_not)
+
+    # Compute integral, that is equal to the sum of the scalar product between the two tensors
+    weighted_sum = torch.sum(mask * saliency)
+    # Normalize by size of the mask
+    cas = weighted_sum / size_mask
+
+    # Compute integral between saliency and mask_not
+    penalization_sum = torch.sum(mask_not * saliency)
+
+    # Normalize by size of the mask_not
+
+    penalization_not = penalization_sum / size_mask_not
+
+    casp = cas - penalization_not
+
+    return casp
+
+
 
 def compute_single_woe_score(P_h_cp, P_h):
 
@@ -70,56 +121,76 @@ class WeightOfEvidence:
 
         absolute_path = os.path.abspath("mask_output")
 
-        dir = os.path.join(absolute_path, dataset_name)
+        dir = os.path.join(absolute_path, extractor_method, dataset_name)
 
-        self.list_concepts = load_list(os.path.join(dir, 'list_concepts.txt'))
+        #self.list_concepts = load_list(os.path.join(dir, 'list_concepts.txt'))
+        self.list_concepts = retrieve_concepts_ordered(dataset_name)
 
         self.dir_mask = os.path.join(dir, 'masks')
-        self.dir_concept = os.path.join(dir, 'concepts')
+        #self.dir_concept = os.path.join(dir, 'concepts')
 
         # Check if the tensor with the probabilities has been already computed
-        output_dir = os.path.abspath('woe_probabilities')
-        tensor_prob_path = os.path.join(output_dir,f"woe_prob_{saliency_method}_{dataset_name}.pth")
-        if os.path.exists(tensor_prob_path):
-            self.prob = torch.load(tensor_prob_path)
-            print(self.prob)
+        output_dir = os.path.abspath('woe_prob')
+
+        #tensor_prob_path = os.path.join(output_dir,model,extractor_method,f"woe_prob_{saliency_method}_{dataset_name}.pth")
+        csv_prob_path = os.path.join(output_dir,model,extractor_method,f"woe_prob_{saliency_method}_{dataset_name}.csv")
+        if os.path.exists(csv_prob_path):
+            self.final_prob = pd.read_csv(csv_prob_path, index_col=0)
+            print("Probability already present")
+            print(self.final_prob)
         else:
 
-            # Compute P(h|cp) for each concept cp and for each class h, and saved each element in the matrix self.prob
-            self.prob = torch.zeros(len(self.list_concepts),
-                              len(self.dataset.classes))  #for each concept I will produce the WoE(h|cp)
+            # Compute P(h|cp) for each concept cp and for each class h, and saved each element in the matrix prob
+            # prob inizializzarlo in pandas dataFrame e salvare in csv e non pth
+            # prob = torch.zeros(len(self.list_concepts),
+            #                   len(self.dataset.classes))  #for each concept I will produce the WoE(h|cp)
+
+            prob = pd.DataFrame(np.zeros((len(self.list_concepts), len(self.dataset.classes))),
+                                     index=self.list_concepts,
+                                     columns=self.dataset.classes)
 
             for idx in range(self.dataset.__len__()):
                 print("IMG " + str(idx))
                 # Get the ground truth bounding boxes and labels
                 image, label = self.dataset.__getitem__(idx)
+                print(f"label: {label}")
 
                 # Retrieve mask
                 masks = load_mask(os.path.join(self.dir_mask, f'mask_{idx}.pth'))
-                concepts = load_mask(os.path.join(self.dir_concept, f'classes_{idx}.pth'))
+                #concepts = load_mask(os.path.join(self.dir_concept, f'classes_{idx}.pth'))
+                print(f"maschera shape:{masks.shape}")
+                #print(concepts.shape)
                 # Retrieve saliency
                 row_sal_info = self.saliency_info.iloc[idx]
                 path_sal = row_sal_info[self.saliency_method+" path"]
                 saliency = load_saliency_map(path_sal)
 
                 # Compute concept presence within the image
-                for mask, concept in zip(masks, concepts):
-                    concept = int(concept.item())
+                for i, mask in enumerate(masks):
+                    concept = self.list_concepts[i]
                     #Apply thresholding to concept presence computed
                     concept_presence = 1 if concept_average_saliency(mask, saliency) > 0.5 else 0
 
-                    self.prob[concept, label] += concept_presence  #
+                    prob.loc[concept, self.dataset.classes[label]] += concept_presence  #
 
 
             # Compute probability of P(h|cp)
-            for label in range(self.prob.shape[1]):
-                for concept in range(self.prob.shape[0]):
+            self.final_prob = pd.DataFrame(np.zeros((len(self.list_concepts), len(self.dataset.classes))),
+                                     index=self.list_concepts,
+                                     columns=self.dataset.classes)
+            for label in range(prob.shape[1]):
+                for concept in range(prob.shape[0]):
                     # Divide each member of the matrix for the number of times in which the concept is present in an image
-                    self.prob[concept, label] = self.prob[concept, label] / self.prob[concept].sum()
+                    if prob.iloc[concept].sum()!=0:
+                        self.final_prob.iloc[concept, label] = prob.iloc[concept, label] / prob.iloc[concept].sum()
+                    else:
+                        print(prob.iloc[concept, label])
+                        print(prob.iloc[concept])
 
-            if not os.path.exists(output_dir):
-                os.makedirs(output_dir)
-            torch.save(self.prob,os.path.join(output_dir,f"woe_prob_{saliency_method}_{dataset_name}.pth"))
+            if not os.path.exists(os.path.join(output_dir,model,extractor_method)):
+                os.makedirs(os.path.join(output_dir,model,extractor_method))
+            #torch.save(prob, tensor_prob_path)
+            self.final_prob.to_csv(csv_prob_path)
 
     def compute_score(self, concepts_specified, labels_specified):
 
@@ -139,11 +210,11 @@ class WeightOfEvidence:
                     print(f"concept {concept} not in list {concepts_specified}")
                     continue
 
-                P_h_cp = self.prob[concept_id, label_id]
+                P_h_cp = self.final_prob.iloc[concept_id, label_id]
 
                 # Count of number of images belonging to class "label" in the dataset and then divided
                 # by total number of image, to retrieve the probability to belong to a certain class
-                P_h = self.saliency_info["True Class"].value_counts().get(label_id) / self.dataset.__len__()
+                P_h = self.saliency_info["True Class"].value_counts().iloc[label_id] / self.dataset.__len__()
                 woe_label = woe_label + compute_single_woe_score(P_h_cp, P_h)
 
             woe_score[label_id] = woe_label